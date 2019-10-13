fef689ac25ede2edf893ddbc80591c4b3af59e78
Ramesh B G <bgramesh8787@gmail.com>
2019-10-13 16:45:27 +0530

    Add project files.

---
+++ FyleBankSearchTask/Search.aspx
@@ -0,0 +1,40 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeBehind="Serach.aspx.cs" Inherits="FyleTask.Serach" %>
+
+<!DOCTYPE html>
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+<head runat="server">
+    <link href="Content/DataTables/css/jquery.dataTables.min.css" rel="stylesheet" />
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <script src="Scripts/jquery-3.4.1.min.js"></script>
+    <script src="Scripts/DataTables/jquery.dataTables.min.js"></script>
+    <script src="Scripts/bootstrap.min.js"></script>
+    <title></title>
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $('#gvdisplay').dataTable();
+        })
+    </script>
+    <style type="text/css">
+       #form1{
+           background-image: url("Images/pexels-photo-1445416.jpeg");
+           background-repeat:no-repeat;
+           background-size:cover;
+           background-attachment:fixed;
+       }
+     
+       #gvdisplay{
+           background-color:gainsboro;
+       }
+    </style>
+</head>
+<body>
+    <form id="form1" runat="server">
+        <div class="container">
+            <br />
+            <strong>Select Bank</strong><asp:DropDownList ID="ddlselectbank" runat="server" CssClass="form-control col-md-3" AutoPostBack="true" OnSelectedIndexChanged="ddlcity_SelectedIndexChanged"></asp:DropDownList><br />
+            <asp:GridView ID="gvdisplay" CssClass="table table-bordered" runat="server"  ></asp:GridView>
+        </div>
+    </form>
+</body>
+</html>
---
+++ FyleBankSearchTask/Search.aspx.cs
@@ -0,0 +1,67 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Script.Serialization;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace FyleTask
+{
+    public partial class Serach : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            if (!IsPostBack)
+            {
+                List<string> lstbank = new List<string>();
+                lstbank.Add("--select--");
+                lstbank.Add("ABHYUDAYA COOPERATIVE BANK LIMITED");
+                lstbank.Add("THE ROYAL BANK OF SCOTLAND N V");
+                lstbank.Add("BASSEIN CATHOLIC COOPERATIVE BANK LIMITED");
+                lstbank.Add("CENTRAL BANK OF INDIA");
+                lstbank.Add("HDFC BANK");
+                ddlselectbank.DataSource = lstbank;
+                ddlselectbank.DataBind();
+
+                DataTable dt = GetData();
+                gvdisplay.DataSource = dt;
+                gvdisplay.DataBind();
+                gvdisplay.HeaderRow.TableSection = TableRowSection.TableHeader;
+            }
+        }
+
+
+        public DataTable GetData()
+        {
+            var json = string.Empty;
+            using (WebClient wc = new WebClient())
+            {
+                json = wc.DownloadString("https://vast-shore-74260.herokuapp.com/banks?city=MUMBAI");
+            }
+            DataTable dt = JsonConvert.DeserializeObject<DataTable>(json);
+            Session["data"] = dt;
+            return dt;
+        }
+
+        protected void ddlcity_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string selectedbank = ddlselectbank.SelectedValue;
+            DataTable dt = (DataTable)Session["data"];
+            DataTable dtf = dt.Clone();
+            foreach (DataRow dr in dt.Rows)
+            {
+                if (dr[7].ToString() == selectedbank)
+                {
+                    dtf.ImportRow(dr);
+                }
+            }
+            gvdisplay.DataSource = dtf;
+            gvdisplay.DataBind();
+            gvdisplay.HeaderRow.TableSection = TableRowSection.TableHeader;
+        }
+    }
+}
